5bb35b583b266d8a252eb1f5b68283d2d8292477
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-12-08 22:01:38 +0600

    ready for deploy

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import SettingBar from "@/components/setting";
 
 export default function Home() {
   return (
-    <section className="container mx-auto">
-      <div className="grid grid-cols-4 justify-center items-start gap-10">
+    <section className="container mx-auto px-5">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-center items-start gap-10">
         <SettingBar />
         <Description />
         <Code />
